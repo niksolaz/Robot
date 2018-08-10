e9f946f246370b98e51762ce746d9bf6b4b1d1bc
Nicola Solazzo <niksolaz@iMac-di-Nicola.local>
2018-08-10 15:14:08 +0200

    two little project unity

---
+++ Robot/Assets/RotateSturtruppen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSturtruppen : MonoBehaviour {
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (0, 180, 0) * Time.deltaTime);
+	}
+		
+}
---
+++ Robot/Assets/Script/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countTextSturtrup;
+	public Text countTextBlocked;
+	public Text winText;
+	public Text loseText;
+
+	private Rigidbody rb;
+	private int countBlock;
+	private int countSturtrup;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+		countBlock = 2;
+		countSturtrup = 0;
+		SetCountText ();
+		winText.text = "";
+		loseText.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter (Collider other) {
+		
+		if (other.gameObject.CompareTag("Sturtruppen"))
+		{
+			other.gameObject.SetActive(false);
+			countSturtrup = countSturtrup + 1;
+			SetCountText ();
+		}
+		if (other.gameObject.CompareTag("Blocked"))
+		{
+			other.gameObject.SetActive(false);
+			countBlock = countBlock - 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countTextSturtrup.text = "Count: " + countSturtrup.ToString ();
+		if (countSturtrup >= 3)
+		{
+			loseText.text = "You Lose! The Empire won! ";
+			winText.text = "";
+			countBlock = 2;
+		}
+		countTextBlocked.text = "Count: " + countBlock.ToString ();
+		if (countBlock == 0)
+		{
+			winText.text = "You Win! Thank you BB-8! You saved the rebellion! ";
+			loseText.text = "";
+			countSturtrup = 0;
+		}
+	}
+}
+	
