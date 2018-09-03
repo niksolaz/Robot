35912be5a14fe3ae9c7547135b8e19f82b9725b6
Nicola Solazzo <niksolaz@iMac-di-Nicola.local>
2018-09-03 16:33:09 +0200

    Game tutorial Sparatutto

---
+++ Sparatutto/Assets/Standard Assets/Scripts/Level1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1 : MonoBehaviour {
+
+	// respond on collisions
+	void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			GameManager.gm.Level1();
+			Debug.Log ("AAAAAAAAAAAA");
+		}
+	}
+}
