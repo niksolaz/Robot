8e290da7eac4a13a35d621b2587eb2ae86803b14
Nicola Solazzo <niksolaz@iMac-di-Nicola.local>
2018-09-07 10:02:33 +0200

    First Commit Walkman game

---
+++ WalkMan/Assets/Scripts/Stoped.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Stoped : MonoBehaviour {
+	
+	void OnCollisionEnter ( Collision collision)
+	{
+		if (collision.gameObject.name == "Player")
+		{
+			//Application.LoadLevel ("Scene1");
+			SceneManager.LoadScene("Scene1");
+		} 
+	} 
+}
